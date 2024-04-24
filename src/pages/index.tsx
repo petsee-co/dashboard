5511c29db4da8338b07f1efa3c45cfffc635b01f
Pauline Hoopoe <paelynn@petsee.co>
2024-04-24 21:14:10 +0200

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,104 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { getSession, getAccessToken } from "@auth0/nextjs-auth0";
+import { useRouter } from "next/router";
 
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
+import { GetServerSidePropsContext } from "next";
 
-export default function Home() {
+interface IndexProps {
+  accessToken: string;
+  idToken: string;
+  user: any;
+}
+
+export default function Index({ accessToken, idToken, user }: IndexProps) {
+  const router = useRouter();
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative flex h-screen bg-blue-50">
+      <div className="relative flex flex-col flex-1 h-full max-h-full overflow-y-scroll">
+        <header className="sticky top-0 flex items-center flex-shrink-0 w-full h-full bg-opacity-100 bg-blue-50 max-h-14 px-2 lg:px-5">
+          <div className="flex items-center justify-between flex-1">
+            <a
+              href="#"
+              className="flex-shrink-0 text-2xl font-bold tracking-widest text-blue-800 uppercase"
+            >
+              Petsee
+            </a>
+
+            <nav className="relative flex items-center justify-end flex-1">
+              <ul className="flex items-center justify-center  space-x-2 ">
+                <li className="relative">
+                  <div
+                    className="absolute inset-0 flex items-center justify-center"
+                    style={{ zIndex: -1 }}
+                  >
+                    <span className="block w-5 h-5 bg-red-600 rounded-full animate-ping"></span>
+                  </div>
+                  <a
+                    href="https://github.com/petsee-co"
+                    target="_blank"
+                    className="inline-flex p-2 bg-blue-200 rounded-full bg-opacity-20 hover:bg-blue-300 hover:bg-opacity-20"
+                    style={{ backdropFilter: "blur(14px)" }}
+                  >
+                    <svg
+                      className="w-6 h-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                    </svg>
+                  </a>
+                </li>
+                <li>
+                  <button
+                    className="p-px border-2 border-blue-100 rounded-full w-11 h-11"
+                    type="button"
+                    onClick={() => {
+                      router.push("/api/auth/logout");
+                    }}
+                  >
+                    <img
+                      className="block object-cover rounded-full"
+                      src={user.picture}
+                      alt={user.name}
+                    />
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <div className="flex flex-col flex-1 max-h-full pl-2 pr-2 rounded-md xl:pr-4">
+          <main className="flex-1 pt-2">
+            <div className="grid grid-cols-1 gap-6 mt-4 md:grid-cols-2">
+              <div className="flex p-4 w-full min-h-48 bg-white rounded-md shadow-md flex-col">
+                <span className="text-sm font-bold text-gray-900 uppercase">
+                  access token
+                </span>
+                <div className="text-gray-900 break-all	mt-2">{accessToken}</div>
+              </div>
+              <div className="flex p-4 w-full min-h-48 bg-white rounded-md shadow-md flex-col">
+                <span className="text-sm font-bold text-gray-900 uppercase">
+                  id token
+                </span>
+                <div className="text-gray-900 break-all	mt-2">{idToken}</div>
+              </div>
+            </div>
+          </main>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
+}
+
+export async function getServerSideProps(ctx: GetServerSidePropsContext) {
+  const session = await getSession(ctx.req, ctx.res);
+  const { user, accessToken, idToken } = session!;
+
+  const { accessToken: accessToken2 } = await getAccessToken(ctx.req, ctx.res);
+
+  console.log("accessToken - 1", accessToken);
+  console.log("accessToken - 2", accessToken2);
+
+  return { props: { accessToken, idToken, user } };
 }
